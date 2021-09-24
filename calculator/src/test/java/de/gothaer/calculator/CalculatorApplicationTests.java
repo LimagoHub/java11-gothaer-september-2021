39546758574d08217e4a4a3c4e57a8f6f93de7d7
Jo Wagner <jowagner@limago.de>
2021-09-24 13:40:06 +0200

    Initial commit

---
+++ calculator/src/test/java/de/gothaer/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gothaer.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
